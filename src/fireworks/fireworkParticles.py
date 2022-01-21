8017ff791ad14f30eedc18a19fb116961a2c2f41
JasonSatherr <jsather@ucsd.edu>
2022-01-20 18:16:09 -0800

    Add documentation

--- src/fireworks/fireworkParticles.py
+++ src/fireworks/fireworkParticles.py
@@ -4,6 +4,8 @@ import numpy as np
 
 '''
 Class that manages positions and velocity of fireworks
+
+Implement acceleration later (negative acc)
 '''
 class FireworkParticles:
 
@@ -18,13 +20,18 @@ class FireworkParticles:
         #The rand normal velocities of each particle
         self.particleVelocities = .08*np.random.randn(len(self.particlesX), 2)
 
+    '''
+    Updates the positions of the firework's particles
+    '''
     def update(self):
         self.updateX()
         self.updateY()
 
+    '''Updates the x positions of the firework's particles'''
     def updateX(self):
         self.particlesX = self.particlesX[:] + self.particleVelocities[:, 0]
 
+    '''Updates the y positions of the firework's particles'''
     def updateY(self):
         self.particlesY = self.particlesY[:] + self.particleVelocities[:, 1]
     
