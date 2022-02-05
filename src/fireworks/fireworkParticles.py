e89992eff6d639100813115fe58fafa0825e9ac3
JasonSatherr <jsather@ucsd.edu>
2022-02-05 14:36:50 -0800

    Added realistic velocity

--- src/fireworks/fireworkParticles.py
+++ src/fireworks/fireworkParticles.py
@@ -20,6 +20,42 @@ class FireworkParticles:
         #The rand normal velocities of each particle
         self.particleVelocities = .08*np.random.randn(len(self.particlesX), 2)
 
+        #new particles velocity implementation
+        xVel , yVel = self.__getInitVelComponents()
+        self.particleVelocities = np.block([[xVel],[yVel]])
+        self.particleVelocities =.16* self.particleVelocities.transpose()
+        print(self.particleVelocities)
+
+    '''
+    Creates an array or initial speeds for the particles
+    '''
+    def __getInitSpeed(self):
+        speeds = np.random.normal(loc = 1, scale = .3, size = len(self.particlesX))
+        return speeds
+
+    '''
+    creates arrays representing the x and y components of the speed
+    '''
+    def __getInitSpeedComponents(self):
+        speeds = self.__getInitSpeed()
+        #take a random chunk of the speed, give it to X
+        xSpeed  = np.random.uniform(low = 0, high  = speeds, size = len(speeds))
+        #assign the rest to Y
+        ySpeedSquared  = np.square(speeds) - np.square(xSpeed)
+        ySpeed = np.sqrt(ySpeedSquared)
+        return xSpeed, ySpeed
+
+    '''
+    Gets velocity (directon) of particles
+    '''
+    def __getInitVelComponents(self):
+        xSpeed, ySpeed = self.__getInitSpeedComponents()
+        signsX = np.random.choice(a = [-1,1], size = len(xSpeed))  #potential error here :(
+        signsY = np.random.choice(a = [-1,1], size = len(ySpeed))  #potential error here :(
+        xVel = xSpeed * signsX
+        yVel = ySpeed * signsY
+        return xVel, yVel
+
     '''
     Updates the positions of the firework's particles
     '''
